e759b2b57d4c37d3774ec6cd94af8ecbb08587a4
diemarg <gochezdiego07@gmail.com>
2019-12-02 13:24:23 -0600

    test4

--- src/test/java/com/mayab/calidad/funcionales/MenCrudTest.java
+++ src/test/java/com/mayab/calidad/funcionales/MenCrudTest.java
@@ -58,7 +58,7 @@ public class MenCrudTest {
         URL = "https://mern-crud.herokuapp.com/";
         
         //travis
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriverr");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         driver = new ChromeDriver(); 
 
         /*
